14fbe4f5aa1971ea9e8b2d8b9c455e630fa0fc9c
Ronaldo Silva <ronaldo.silva.jr96@gmail.com>
2023-07-03 12:28:09 -0300

    page: home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,45 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="">I will work on</label>
+          <TaskInput id="task" placeholder="Name your project" />
+
+          <label htmlFor="">during</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+          />
+
+          <span>minutes.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Start
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
