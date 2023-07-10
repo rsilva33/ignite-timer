52541dd35736d581ea7243b49b23a055583ed29d
Ronaldo Silva <ronaldo.silva.jr96@gmail.com>
2023-07-10 12:42:54 -0300

    reset form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -25,15 +25,18 @@ const newCycleFormValidationSchema = zod.object({
 // Controlled -> manter em tempo real a informacao de input que o usuario prenche no estado atual guardado / Formulario simples com poucos campos
 // Uncontroled -> nao monitora o valor digitado em tempo real / dashboard com diversos campos de input
 
-interface NewCycleFormData {
-  task: string
-  minutesAmount: number
-}
+// interface NewCycleFormData {
+//   task: string
+//   minutesAmount: number
+// }
+
+// nao pode utilizar uma variavel javascript direto no ts por isso e necessario converter utilizando typeof
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
   // register -> adiciona um input ao formulario
 
-  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+  const { register, handleSubmit, watch, reset } = useForm({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
@@ -43,6 +46,7 @@ export function Home() {
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
+    reset()
   }
 
   // saber o o valor do campo de task em tempo real
@@ -68,7 +72,7 @@ export function Home() {
             <option value="3" />
           </datalist>
 
-          <label htmlFor="">during</label>
+          <label htmlFor="minutesAmount">during</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
